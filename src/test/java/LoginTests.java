7599fdf4667aee7f2d29bded9a01a8176ea060e8
Vijendar-123 <vijendar@testpro.io>
2025-09-19 22:37:13 -0400

    homerwork-16
    homerwork-16

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -14,14 +16,72 @@ public class LoginTests extends BaseTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
-        WebDriver driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver(options);//creating an object called driver
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         // TODO (for students): Review the configuration as part of HW15
 
-        String url = "httpps://qa.koel.app/";
+        String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
+@Test
+public void loginValidEmailPassowrd() throws InterruptedException {
+
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+    //precondition
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+     String url= "https://qa.koel.app/";
+     driver.get(url);
+
+     //test steps
+     WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+     emailField.clear();
+     emailField.sendKeys("vijendar@testpro.io");
+     WebElement passowrdField = driver.findElement(By.cssSelector("input[type='password']"));
+     passowrdField.clear();
+     passowrdField.sendKeys("cnb243YB");
+     WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+     submitBtn.click();
+     Thread.sleep(2000);
+
+     //Expected Result
+     WebElement avatarIcon = driver.findElement(By.cssSelector("img.avatar"));
+     Assert.assertTrue(avatarIcon.isDisplayed());//test pass only if the input is true
+
+    driver.quit();
 }
+
+@Test
+public void incorrectEmailCorrectPassowrd() throws InterruptedException {
+    ChromeOptions options = new ChromeOptions();
+    options.addArguments("--remote-allow-origins=*");
+    //precondition
+    WebDriver driver = new ChromeDriver(options);
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    String url= "https://qa.koel.app/";
+    driver.get(url);
+
+    //test Steps
+    WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+    emailField.clear();
+    emailField.sendKeys("vijendar@testpro.io");
+    WebElement passowrdField = driver.findElement(By.cssSelector("input[type='password']"));
+    passowrdField.clear();
+    passowrdField.sendKeys("");
+    WebElement submitBtn = driver.findElement(By.cssSelector("button[type='submit']"));
+    submitBtn.click();
+    Thread.sleep(2000);
+
+    //Expected Result
+    Assert.assertEquals(driver.getCurrentUrl(),url);
+
+    driver.quit();
+
+
+}
+}
+
