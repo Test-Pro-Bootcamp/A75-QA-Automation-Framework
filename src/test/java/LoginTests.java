314fed6bb88e93901147788d9dd19bf73eb08125
wiles42 <wiley.griffin@testpro.io>
2025-10-13 17:15:05 -0400

    homework-23
    homework-23

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -7,23 +7,20 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import pagefactory.BasePage;
+import pagefactory.HomePage;
+import pagefactory.LoginPage;
 
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
     @Test
-    public void loginEmptyEmailPassword() {
+public void loginValidEmailPassword(){
+        LoginPage loginpage = new LoginPage(driver);
+        HomePage homePage = new HomePage(driver);
 
-//      Added ChromeOptions argument below to fix websocket error
-       // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        WebElement avatarIcon = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='logo']")));
-
-
-        // TODO (for students): Review the configuration as part of HW15
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.quit();
+        loginpage.provideEmail("wiley.griffin@testpro.io")
+                .providePassword("KUgY8Y1p")
+                .clickSubmit();
     }
 }
